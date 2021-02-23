0bd83c125e95a99129b3975c499248744bfc5b9a
chris <christoph.langer100@gmail.com>
2021-02-23 20:39:34 +0100

    add pace loader

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,7 +1,27 @@
-window.addEventListener('scroll', function(e) {
+var element = document.getElementById('content-wrapper');
+
+element.addEventListener('scroll', function(e) {
+
+    var height = $(window).height(); 
+    var offset = scrollable.scrollTop;
 
     var matches = document.querySelectorAll('img[data-parallax-speed]');
-    var scrolled = window.pageYOffset;
+
+    matches.forEach(function(element, index, array) {
+
+        const speed = element.getAttribute('data-parallax-speed');
+        var rate = offset * -speed;
+
+        element.style.transform = 'translate3d(0px, ' + rate + 'px, 0px)';
+    });
+
+});
+
+/*
+element.addEventListener('scroll', function(e) {
+
+    var matches = document.querySelectorAll('img[data-parallax-speed]');
+    var scrolled = element.pageYOffset;
 
     matches.forEach(function(element, index, array) {
 
@@ -12,3 +32,4 @@ window.addEventListener('scroll', function(e) {
     });
 
 });
+*/
